b6ad911b59d94e7f1f421e7ddd7848fa6f8acf7d
folkforms <alan.bergin@gmail.com>
2021-12-03 11:06:08 +0000

    Load recipes from json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import recipes from "./recipes";
+
+console.log(`Found ${recipes.length} recipes`);
+recipes.forEach(r => console.log(r.recipe));
 
 function App() {
   return (
@@ -15,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Found {recipes.length} recipes
         </a>
       </header>
     </div>
